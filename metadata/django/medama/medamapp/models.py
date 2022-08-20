601755f065ddc7fee7df4cf53960e1ff30041e60
Matthias <mafisc@gmail.com>
2022-08-20 13:23:38 +0200

    feature(metadata): WIP data models and templates in django

--- metadata/django/medama/medamapp/models.py
+++ metadata/django/medama/medamapp/models.py
@@ -1,6 +1,9 @@
+import uuid
+from uuid import uuid4
 from django.db import models
 
 class User(models.Model):
+    user_external_id = models.UUIDField(max_length=16, default=uuid.uuid4) # UUID
     username = models.CharField(max_length=100) # PII!
     email = models.CharField(max_length=200) # PII!
     password = models.CharField(max_length=100)
@@ -11,7 +14,7 @@ class User(models.Model):
         return str("id: " + str(self.id) + ", username: " + self.username)
 
 class Location(models.Model):
-    location_id = models.CharField(max_length=16) # UUID
+    location_external_id = models.UUIDField(max_length=16, default=uuid.uuid4) # UUID
     latitude = models.FloatField()
     longitude = models.FloatField()
     altitude = models.IntegerField() # m
@@ -22,10 +25,27 @@ class Location(models.Model):
         return str("id: " + str(self.id) + ", lat: " + str(self.latitude) + ", lon: " + str(self.longitude) + ", alt: " + str(self.altitude) + ", depth: " + str(self.depth) + ", soil: " + self.soil_type)
 
 class Sensor(models.Model):
+    device_external_id = models.UUIDField(max_length=16, default=uuid.uuid4) # UUID
     device_id = models.CharField(max_length=100)
     label = models.CharField(max_length=200)
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    SENSOR_STATUS = (
+        ('m', 'Maintenance'), # configured, but temp out of order due to maintenance
+        ('o', 'Out-of-order'), # configured, but not working
+        ('d', 'Deployed'), # configured and working
+        ('n', 'New'), # not yet configured in ttn
+        ('r', 'Ready'), # configured in TTN but not yet deployed
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=SENSOR_STATUS,
+        blank=True,
+        default='n',
+        help_text='Senor status',
+    )
 
     def __str__(self):
         return str("device_id: " + str(self.device_id) + ", label: " + self.label)
--- metadata/django/medama/medamapp/views.py
+++ metadata/django/medama/medamapp/views.py
@@ -1,14 +1,20 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 
+from .models import Sensor
+
 def index(request):
     return HttpResponse("Hello, world. You're at the medama app index.")
 
 def login(request):
     return HttpResponse("Login form here.")
 
-def user_landingpage(request):
+def user_landingpage(request, user_id):
     return HTTPResponse("This is where logged in users can see infos about their sensor(s)")
 
-def user_register_sensor(request):
+def user_profile(request, user_id):
+    return HTTPResponse("This is where logged in users can see infos about their sensor(s)")
+
+# TODO: This is not immediately required. Sensors can be registered by codeforbi staff in the beginning
+def user_register_sensor(request, user_id):
     return HTTPResponse("This is where logged in users can add more sensors")
