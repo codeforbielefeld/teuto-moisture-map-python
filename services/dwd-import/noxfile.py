71ae091ec42d6fcf9221160761b4bf5ae74f2cb0
bnord <bnord01@gmail.com>
2022-10-16 14:53:40 +0200

    format

--- services/dwd-import/noxfile.py
+++ services/dwd-import/noxfile.py
@@ -3,18 +3,18 @@ import nox
 
 @nox.session(python=["3.10"])
 def tests(session):
-    session.run("poetry", "install", external = True)
-    session.run("poetry", "run", "pytest", external= True)
+    session.run("poetry", "install", external=True)
+    session.run("poetry", "run", "pytest", external=True)
 
 
 @nox.session(python=["3.10"])
 def lint(session):
-    session.run("poetry", "install", external= True)
-    session.run("poetry", "run", "black", "--check", ".", external= True)
-    session.run("poetry", "run", "flake8", ".", external= True)
+    session.run("poetry", "install", external=True)
+    session.run("poetry", "run", "black", "--check", ".", external=True)
+    session.run("poetry", "run", "flake8", ".", external=True)
 
 
 @nox.session(python=["3.10"])
 def typing(session):
-    session.run("poetry", "install", external= True)
-    session.run("poetry", "run", "mypy", ".", external= True)
+    session.run("poetry", "install", external=True)
+    session.run("poetry", "run", "mypy", ".", external=True)
