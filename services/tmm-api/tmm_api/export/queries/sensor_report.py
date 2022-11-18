f614bec4220ddda3f25411ed8dc93f1a18b23b42
bnord <bnord01@gmail.com>
2022-11-18 17:11:49 +0100

    Lazy loading

--- services/tmm-api/tmm_api/export/queries/sensor_report.py
+++ services/tmm-api/tmm_api/export/queries/sensor_report.py
@@ -1,11 +1,17 @@
 import os
 
-query_file = os.path.join(os.path.dirname(__file__), "sensor_report.flux")
-with open(query_file, "r") as file:
+_query: str | None = None
+
+
+def _get_query() -> str:
     global _query
-    _query = file.read()
+    if _query is None:
+        query_file = os.path.join(os.path.dirname(__file__), "sensor_report.flux")
+        with open(query_file, "r") as file:
+            _query = file.read()
+    return _query
 
 
 def query(sensor: str, bucket: str):
     p = {"_bucket": bucket, "_device": sensor}
-    return _query, p
+    return _get_query(), p
--- services/tmm-api/tmm_api/export/sensor_report.py
+++ services/tmm-api/tmm_api/export/sensor_report.py
@@ -5,11 +5,18 @@ from tmm_api.common.secrets import get_secret_or_fail
 from tmm_api.export.util import influx_table_daily_values_to_dict
 from .queries.sensor_report import query
 
-bucket = get_secret_or_fail("TMM_BUCKET")
+_bucket: str | None = None
+
+
+def get_bucket() -> str:
+    global _bucket
+    if _bucket is None:
+        _bucket = get_secret_or_fail("TMM_BUCKET")
+    return _bucket
 
 
 def sensor_report(sensor: str) -> str:
     with get_influx_client() as client:
-        q, p = query(sensor, bucket)
+        q, p = query(sensor, get_bucket())
         tables = client.query_api().query(query=q, params=p)
         return json.dumps(influx_table_daily_values_to_dict(tables))
