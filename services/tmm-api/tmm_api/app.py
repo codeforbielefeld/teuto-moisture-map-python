8956c0b51015ae29abf13b4eb2dc0894ac0711ce
bnord <bnord01@gmail.com>
2023-08-06 12:29:33 +0200

    Log bad requests

--- services/tmm-api/tmm_api/app.py
+++ services/tmm-api/tmm_api/app.py
@@ -1,8 +1,15 @@
 # app.py
+from logging import StreamHandler, getLogger
+import logging
 from threading import Lock
 from cachetools import TTLCache, cached
-from fastapi import FastAPI, Form, Header, Query, Path
+from fastapi import FastAPI, Form, Header, Query, Path, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.exception_handlers import (
+    request_validation_exception_handler as _request_validation_exception_handler,
+)
 from fastapi.responses import HTMLResponse, PlainTextResponse, JSONResponse
+
 from tmm_api.common.auth import get_digest, is_auth
 from tmm_api.export.sensor_report import ReportResolution, SensorReport, sensor_report
 from tmm_api.domain.SoilMeasurement import SoilMeasurement
@@ -17,13 +24,26 @@ from tmm_api.ttn.test_data import write_test_data
 
 app = FastAPI(title="BodenfeuchteAPI")
 
+logger = getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(StreamHandler())
+logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
+
+
 # =====================
 # Webhook web interface
 # =====================
+
+
 write_enabled = os.environ.get("ENABLE_WRITE")
 if write_enabled == "true":
 
-    @app.post("/measurement/ttn", status_code=201, response_model=SoilMeasurement, responses={401: {}})
+    @app.post(
+        "/measurement/ttn",
+        status_code=201,
+        response_model=SoilMeasurement,
+        responses={401: {}},
+    )
     def ttn_dragino(message: TTNMessage, TMM_APIKEY: str = Header()):  # noqa: B008,N803
         if not is_auth(message.end_device_ids.device_id, TMM_APIKEY):
             return JSONResponse(status_code=401, content={"error": "Unauthorized"})
@@ -59,9 +79,22 @@ def sensor_data(
     return sensor_report(sensor, records=records, resolution=resolution)
 
 
+# ==================
+# Exception handlers
+# ==================
+
+
+@app.exception_handler(RequestValidationError)
+async def request_validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    body = exc.body
+    logger.info(f"Request validation failed.\n Path: {request.url},\n Errors: {exc.errors()},\n Body: {body}")
+    return await _request_validation_exception_handler(request, exc)
+
+
 # =====================
 # Development utilities
 # =====================
+
 
 development_mode = os.environ.get("DEVELOPMENT_MODE")
 if development_mode == "true":
