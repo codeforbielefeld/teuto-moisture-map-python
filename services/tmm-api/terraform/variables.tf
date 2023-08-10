d4378f867d11035bf46ef9e4cd1c64a4838eb52f
bnord <bnord01@gmail.com>
2023-08-10 14:28:46 +0200

    Deploy lambda code without terraform

--- services/tmm-api/terraform/variables.tf
+++ services/tmm-api/terraform/variables.tf
@@ -3,8 +3,6 @@ locals {
   
   lambda_function_name = "tmm_api_lambda"
 
-  lambda_function_package = "lambda_package.zip"
-
   lambda_function_handler = "tmm_api.handler"
 
   lambda_log_retention = 7
